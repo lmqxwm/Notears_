9c52949a53beb81355cdebfc7b0c3593b9072b4a
lmqxwm <mqliu@uchicago.edu>
2023-09-12 12:50:10 -0500

    modified:   notears/exp6.py

--- notears/exp6.py
+++ notears/exp6.py
@@ -10,6 +10,7 @@ import time
 from itertools import permutations
 import random
 from functools import partial
+import linear2
 
 # d (int): num of nodes
 # s0 (int): expected num of edges
@@ -28,7 +29,7 @@ def estimate_once(d, graph_type, sem_type, seed=1, lambda1=0, loss_type='l2', BB
     
     B_true = utils.simulate_dag(d, s0, graph_type)
     W_true = utils.simulate_parameter(B_true)
-    X = utils.simulate_linear_sem(W_true, n, sem_type, noise_scale=np.random.uniform(1, 5, d))
+    X = utils.simulate_linear_sem(W_true, n, sem_type, noise_scale=np.random.uniform(0.5, 5, d))
 
     B = np.min([math.factorial(d), BB])
     results = np.zeros([5, B+2])
@@ -46,11 +47,12 @@ def estimate_once(d, graph_type, sem_type, seed=1, lambda1=0, loss_type='l2', BB
     
     results.to_csv("./results_loss2/result_"+str(seed)+"_"+sem_type+"_"+str(d)+".csv", index=False)
 
-def estimate_template(d, sem_type, seed=1, lambda1=0, loss_type='l2', BB=100):
+def estimate_template(sem_type, seed=1, lambda1=0, loss_type='l2', BB=100):
     print("=============processing seed=", seed, "================")
+    d = 12
     #utils.set_random_seed(seed)
-    n = d-1
-    s0 = n
+    n = 2 * d
+    s0 = d-1
     print("n, d, s0, em_type, lambda1, loss_type", "-", n, d, s0, sem_type, lambda1, loss_type)
     
     B_true = np.zeros([12, 12])
@@ -59,21 +61,57 @@ def estimate_template(d, sem_type, seed=1, lambda1=0, loss_type='l2', BB=100):
             B_true[i-1, i] = 1
             B_true[i-2, i] = 1
     W_true = utils.simulate_parameter(B_true)
-    X = utils.simulate_linear_sem(W_true, n, sem_type, noise_scale=np.random.uniform(1, 5, d))
+    X = utils.simulate_linear_sem(W_true, n, sem_type, noise_scale=np.random.uniform(0.5, 3, d))
 
-    B = np.min([math.factorial(d), BB])
+    # B = np.min([math.factorial(d), BB])
+    B = 1296
     results = np.zeros([5, B+2])
     results[0, 0] = 0.5 / X.shape[0] * ((X-X@W_true) ** 2).sum()
     #all_perm = list(permutations(range(X.shape[1])))
     #utils.set_random_seed(seed+d)
     inds = [i for i in range(d)]
     results[:, 1] = linear.notears_linear(X, W_true=W_true, lambda1=0, loss_type=loss_type)
-    for i in range(B):
-         random.shuffle(inds)
-         results[:, i+2] = linear.notears_linear(X[:, inds], W_true=W_true, lambda1=0, loss_type="l2")
+    count = 0
+    for a1 in permutations([0,1,2]):
+        for a2 in permutations([3,4,5]):
+            for a3 in permutations([6,7,8]):
+                for a4 in permutations([9,10,11]):
+                    count += 1
+                    new_ind = list(a1) + list(a2) + list(a3) + list(a4)
+                    results[:, count+1] = linear.notears_linear(X[:, new_ind], W_true=W_true, lambda1=0, loss_type="l2")
 
     results = pd.DataFrame(results.T, columns=[
     "loss_est", "loss_l1", "obj_aug", "obj_dual", "h"])
+    
+    results.to_csv("./results_loss2/result_"+str(seed)+"_"+sem_type+"_"+str(d)+".csv", index=False)
+
+def estimate_once_dagma(d, graph_type, sem_type, seed=1, lambda1=0, loss_type='l2', BB=100):
+    print("=============processing seed=", seed, "================")
+    #utils.set_random_seed(seed)
+    n = d-1
+    s0 = n
+    print("n, d, s0, graph_type, sem_type, lambda1, loss_type", "-", n, d, s0, graph_type, sem_type, lambda1, loss_type)
+    
+    B_true = utils.simulate_dag(d, s0, graph_type)
+    W_true = utils.simulate_parameter(B_true)
+    X = utils.simulate_linear_sem(W_true, n, sem_type, noise_scale=np.random.uniform(0.5, 3, d))
+    
+    model = linear2.DagmaLinear(loss_type='l2')
+    
+
+    B = np.min([math.factorial(d), BB])
+    results = np.zeros(B+2)
+    results[0] = 0.5 / X.shape[0] * ((X-X@W_true) ** 2).sum()
+    #all_perm = list(permutations(range(X.shape[1])))
+    #utils.set_random_seed(seed+d)
+    inds = [i for i in range(d)]
+    results[1] = model.fit(X, lambda1=0)
+    for i in range(B):
+         random.shuffle(inds)
+         results[i+2] = model.fit(X[:, inds], lambda1=0)
+
+    results = pd.DataFrame(results.T, columns=[
+    "loss_est"])
     
     results.to_csv("./results_loss2/result_"+str(seed)+"_"+sem_type+"_"+str(d)+".csv", index=False)
 
@@ -83,11 +121,14 @@ if __name__ == '__main__':
     ds = [5, 10, 15, 20, 25, 30]
     pool = mp.Pool(processes=6)
     with pool:
-        for sm in range(len(semtypes)):
-            pool.map(partial(estimate_once, 
-              graph_type="ER", seed=5010, sem_type=semtypes[sm], lambda1=0), 
-              ds)
+        # for sm in range(len(semtypes)):
+        #     pool.map(partial(estimate_once, 
+        #       graph_type="ER", seed=5022, sem_type=semtypes[sm], lambda1=0), 
+        #       ds)
+        #     pool.map(partial(estimate_once_dagma, 
+        #       graph_type="ER", seed=5023, sem_type=semtypes[sm], lambda1=0), 
+        #       ds)
         pool.map(partial(estimate_template, 
-              d=12, seed=5011, lambda1=0), 
+              seed=5025, lambda1=0), 
               semtypes)
     pool.close()
